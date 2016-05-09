099231aa488987d2a09c2dc1a6b21ae81c3ae0df
Dummy <firemango@outlook.com>
2016-05-09 22:16:28 +0800

    Revert back to previous version

--- AnotherPortfolio5/DummyPortfolio5.cpp
+++ AnotherPortfolio5/DummyPortfolio5.cpp
@@ -6,7 +6,4 @@ using namespace std;
 void main()
 {
 	cout << "Dummy added" << endl;
-	cout << "Added new line to Dummy wrote something new" << endl;
-	cout << "Version 1" << endl;
-	cout << "Added Version 2" << endl;
 }
